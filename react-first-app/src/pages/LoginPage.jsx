56b635188064e189e427c56abfff138131ef3e51
Юношева Катерина <katerinayunosheva@gmail.com>
2023-07-04 07:07:04 +0300

    + Monster :)

--- react-first-app/src/pages/LoginPage.jsx
+++ react-first-app/src/pages/LoginPage.jsx
@@ -1,26 +1,43 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Container, Flex, Img } from '@chakra-ui/react'
 import Header from "../components/Header/Header"
 import EmailAddress from "../components/Login/EmailAddress"
 import Password from '../components/Login/Passvord'
+import Monster from '../assets/Login/image 56.svg'
 
 const LoginPage = () => {
 
+    const Monstr = 
+     <img src= {Monster} alt="Img" />
 
     return (
        <>
         <Header/>
-        <Container maxW= 'xl'>
+        <Container maxW= '4xl'>
             <Flex 
                 display= 'flex'
-                flexDirection= 'column' 
-                gap= '30px'
-            > 
-                <EmailAddress/>
-                <Password/>
+                alignItems= 'center'                
+            >
+                <Container maxW= 'xs'>
+                        {Monstr}
+                    </Container>
+                    <Container maxW= 'xl'>
+                        
+                        <Flex 
+                            display= 'flex'
+                            flexDirection= 'column' 
+                            gap= '30px'
+                        > 
+                            <EmailAddress/>
+                            <Password/>
+                        </Flex>
+                        
+                </Container>  
             </Flex>
+             
+         </Container>     
+        </>
+        
             
-        </Container>        
-       </>
     )
 }
 
